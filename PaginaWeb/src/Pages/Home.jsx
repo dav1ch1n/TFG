8db3ed5b3300134e16a98ce8872b16cc6c25801d
dav1ch1n <dgliceras@gmail.com>
2026-04-13 18:29:33 +0200

    prueba
    prueba

---
+++ PaginaWeb/src/Pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1 style={{ marginBottom: '40px' }}>WIKI MEGABONK</h1>
+      <div className="menu-grid">
+        <Link to="/personajes" className="menu-button">
+          <span>👤</span>
+          Personajes
+        </Link>
+        <Link to="/armas" className="menu-button">
+          <span>⚔️</span>
+          Armas
+        </Link>
+        <Link to="/tomos" className="menu-button">
+          <span>📖</span>
+          Tomos
+        </Link>
+        <Link to="/objetos" className="menu-button">
+          <span>🎒</span>
+          Objetos
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
